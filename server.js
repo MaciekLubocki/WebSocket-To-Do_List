c75995977b6e1a902950d2345872317778d6e736
Maciek Lubocki <mail2maciek@gmail.com>
2020-11-18 17:44:38 +0100

    to do list

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+
+const app = express();
+
+const messages = [];
+const users = [];
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+
+  socket.on('message', (message) => {
+    console.log(message);
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('user', (name) => {
+    console.log(`${name} joined the chat`);
+    const userData = {name: name, id: socket.id};
+    users.push(userData);
+    socket.broadcast.emit('message', {name: 'Chat Bot', message: `${name} has joined the conversation!`});
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Oh, socket ' + socket.id + ' has left');
+    const getUser = users.find(name => name.id == socket.id);
+    console.log(getUser);
+    socket.broadcast.emit('message', {name: 'Chat Bot', message: `${getUser.name} left the conversation... :(`});
+    const userIndex = users.indexOf(getUser);
+    users.splice(userIndex, 1);
+    console.log(users);
+  });
+
+  console.log('I\'ve added a listener on message and disconnect events \n');
+});
